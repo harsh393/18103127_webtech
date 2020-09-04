cbe23ca7eae9673843e5eba4e1238cd0b28fa878
harsh393 <54942126+harsh393@users.noreply.github.com>
2020-09-04 23:05:45 +0530

    Add files via upload

---
+++ ques3.java
@@ -0,0 +1,22 @@
+public class ques3 {
+   public static void main(String[] args) {
+      String str = "jdkoepacmbtr";
+      System.out.println("Before Sorting:" + str);
+      int j = 0;
+      char temp = 0;
+      char[] chars = str.toCharArray();
+      for(int i=0; i < chars.length; i++) {
+         for(j=0; j < chars.length; j++) {
+            if(chars[j] > chars[i]) {
+               temp = chars[i];
+               chars[i] = chars[j];
+               chars[j] = temp;
+            }
+         }
+      }
+      System.out.println("After Sorting:");
+      for(int k=0; k < chars.length; k++) {
+         System.out.print(chars[k]);
+      }
+   }
+}
---
+++ ques4.java
@@ -0,0 +1,25 @@
+public class ques4{
+   
+ public static void main(String[] args){
+        
+  int n=1;
+      
+  int sum = 1;
+
+  while(n<Integer.MAX_VALUE){
+   
+         if(sum==n*n){
+           
+         System.out.println("Ans: "+n);
+         
+   }
+         
+   n+=1;
+        
+   sum+=n;
+     
+   }
+       
+   System.out.println("Final loop, sum: "+sum+" n: "+n);
+  
+  }
